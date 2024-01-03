4a202d428d0871f4606156a69bfcd51eb663799d
yukihira-pot <nagohachi.study@gmail.com>
2024-01-04 00:13:09 +0900

    再帰での探索を追加

--- src/solver.hpp
+++ src/solver.hpp
@@ -1,5 +1,9 @@
 #include <algorithm>
 #include <array>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
 
 #include "blocks.hpp"
 #include "field.hpp"
@@ -13,17 +17,50 @@ class Solver {
     // 8 つごとに回転と裏返しで同じブロックを表す.
     // (i.e. j を 8 の倍数として, _blocks[i][j], ..., _blocks[i][j + 8]
     // は回転と裏返しにより一致する)
-    std::array<std::array<Block, TOTAL_BLOCK_SIZE>, PLAYER_SIZE>
-        _blocks{};
+    std::array<std::array<Block, TOTAL_BLOCK_SIZE>, PLAYER_SIZE> _blocks{};
     // 各プレイヤーについて, どのブロックが使用済みかどうかを借りする配列.
     // _used[i][j] := プレイヤー i のブロック j が使用済みかどうか
-    std::array<std::array<bool, TOTAL_BLOCK_SIZE>, PLAYER_SIZE>
-        _used{};
+    std::array<std::array<bool, TOTAL_BLOCK_SIZE>, PLAYER_SIZE> _used{};
 
   public:
     Solver() { setup_blocks(); }
 
   private:
+    /// @brief player を指定し、
+    /// @param player 
+    void place_one_turn(const Player &player) {
+        // すべてブロックを使っていれば return
+        if(is_all_blocks_used()) {
+            auto t = std::time(nullptr);
+            auto tm = *std::localtime(&t);
+            std::ostringstream oss;
+            oss << std::put_time(&tm, "%Y-%m-%d-%H-%M-%S");
+            std::string filename = oss.str();
+            _field.save_to_file("../output/" + filename);
+            return;
+        }
+        for(unsigned short x = 0; x < FIELD_WIDTH; x++) {
+            for(unsigned short y = 0; y < FIELD_WIDTH; y++) {
+                for(unsigned short block_idx = 0; block_idx < TOTAL_BLOCK_SIZE;
+                    block_idx++) {
+                    if(_used[player][block_idx]) {
+                        continue;
+                    }
+                    if(_field.is_able_to_place(x, y, _blocks[player][block_idx],
+                                               player)) {
+                        // ブロックを配置
+                        _field.place(x, y, _blocks[player][block_idx], player);
+                        _used[player][block_idx] = true;
+                        place_one_turn(next_player(player));
+                        // ブロックを削除 (バックトラック)
+                        _field.remove(x, y, _blocks[player][block_idx]);
+                        _used[player][block_idx] = false;
+                    }
+                }
+            }
+        }
+    }
+
     /// @brief 各プレイヤーの保持するブロック集合に対し、各 mode
     /// の各ブロックを割り当てる
     void setup_blocks() {
@@ -39,8 +76,7 @@ class Solver {
         }
     }
 
-    bool
-    is_block_used(const std::array<bool, TOTAL_BLOCK_SIZE> &block) {
+    bool is_block_used(const std::array<bool, TOTAL_BLOCK_SIZE> &block) {
         return std::all_of(block.begin(), block.end(),
                            [](bool y) { return y; });
     }
@@ -48,8 +84,7 @@ class Solver {
     bool is_all_blocks_used() {
         return std::all_of(
             _used.begin(), _used.end(),
-            [this](
-                const std::array<bool, TOTAL_BLOCK_SIZE> &block) {
+            [this](const std::array<bool, TOTAL_BLOCK_SIZE> &block) {
                 return is_block_used(block);
             });
     }
