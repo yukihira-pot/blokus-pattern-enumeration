ff907c6931595d39f1abdb4ec7bd69224b76b561
yukihira-pot <nagohachi.study@gmail.com>
2024-01-03 23:39:22 +0900

    次のプレイヤーを選ぶ関数を実装

--- src/players.hpp
+++ src/players.hpp
@@ -1,3 +1,4 @@
+#pragma once
 constexpr unsigned short PLAYER_SIZE = 4;
 
 enum Player {
@@ -6,3 +7,7 @@ enum Player {
     PLAYER_C = 2, 
     PLAYER_D = 3
 };
+
+Player next_player(Player current) {
+    return static_cast<Player>((static_cast<int>(current) + 1) % PLAYER_SIZE);
+}
